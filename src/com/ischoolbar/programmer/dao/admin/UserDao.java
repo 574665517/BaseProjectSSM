1730074c23b531bbf9afd5894cbd02d7dfa02dcb
574665517 <ywx574665517@163.com>
2020-03-17 20:17:16 +0800

    提交文件

---
+++ src/com/ischoolbar/programmer/dao/admin/UserDao.java
@@ -0,0 +1,21 @@
+package com.ischoolbar.programmer.dao.admin;
+
+import com.ischoolbar.programmer.entity.admin.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * user”√ªßdao
+ */
+@Repository
+public interface UserDao {
+	public User findByUsername(String username);
+	public int add(User user);
+	public int edit(User user);
+	public int editPassword(User user);
+	public int delete(String ids);
+	public List<User> findList(Map<String, Object> queryMap);
+	public int getTotal(Map<String, Object> queryMap);
+}
